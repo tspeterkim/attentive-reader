a7a5ad2ec240f209c1a673b5f38a197ad5d21b01
Peter Kim <peterkim95@ucla.edu>
2017-10-26 00:37:27 -0700

    fix data fetch again but not for the last time

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-DATASETS_DIR = "data/"
-mkdir -p "$DATASETS_DIR"
+DATASETS_DIR = data/
+mkdir -p $DATASETS_DIR
 
-cd "$DATASETS_DIR"
+cd $DATASETS_DIR
 
 # Get CNN/DM Dataset
 wget http://cs.stanford.edu/~danqi/data/cnn.tar.gz
-unzip cnn.tar.gz
+tar xvf cnn.tar.gz
 rm cnn.tar.gz
 
 wget http://cs.stanford.edu/~danqi/data/dailymail.tar.gz
-unzip dailymail.tar.gz
+tar xvf dailymail.tar.gz
 rm dailymail.tar.gz
 
 # Get GloVe vectors (all dims)
 wget http://nlp.stanford.edu/data/glove.6B.zip
-unzip glove.6B.zip
+unzip xvf glove.6B.zip
 rm glove.6B.zip
